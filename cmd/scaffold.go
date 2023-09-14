530c33ab715b7cd162ca987dced16fbc65802252
Erin Borders <48973620+erinborders@users.noreply.github.com>
2023-09-14 09:41:24 -0400

    add initial template code (#12)

--- cmd/scaffold.go
+++ cmd/scaffold.go
@@ -4,25 +4,54 @@ import (
 	"fmt"
 
 	"github.com/azure/spin-aks-plugin/pkg/spin"
+	"github.com/azure/spin-aks-plugin/pkg/templates"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	rootCmd.AddCommand(scaffold)
+	rootCmd.AddCommand(newScaffoldCmd())
+}
+func newScaffoldCmd() *cobra.Command {
+	sc := &templates.ScaffoldCmd{}
+
+	var scaffold = &cobra.Command{
+		Use:   "scaffold",
+		Short: "Generates required Dockerfile and Kubernetes manifests",
+		Long:  "Creates Dockerfile and Kubernetes manifests required to run your application on AKS.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := setConfig(sc); err != nil {
+				return err
+			}
+
+			m, err := spin.Load()
+			if err != nil {
+				return fmt.Errorf("parsing spin: %w", err)
+			}
+
+			fmt.Println(m)
+
+			return nil
+		},
+	}
+
+	f := scaffold.Flags()
+	f.StringVar(&sc.ManifestDestination, "k8s-dest", "", "the destination for the kubernetes manifests")
+	f.StringVarP(&sc.Type, "type", "t", "", "the type of kubernetes manifest to use. Options are Helm, Kustomize, and Kube")
+	f.BoolVar(&sc.Override, "override", false, "override existing Dockerfiles and kubernetes manifests")
+	f.BoolVar(&sc.Defaults, "y", false, "accept all default values")
+	f.StringVarP(&sc.SpinConfig, "config", "c", "", "the location of spin.toml configuration file")
+
+	return scaffold
 }
 
-var scaffold = &cobra.Command{
-	Use:   "scaffold",
-	Short: "Generates required Dockerfile and Kubernetes manifests",
-	Long:  "Creates Dockerfile and Kubernetes manifests required to run your application on AKS.",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		m, err := spin.Load()
-		if err != nil {
-			return fmt.Errorf("parsing spin: %w", err)
-		}
+func setConfig(sc *templates.ScaffoldCmd) error {
+	if err := sc.InitConfig(); err != nil {
+		return err
+	}
 
-		fmt.Println(m)
+	if err := sc.ValidateConfig(); err != nil {
+		return err
+	}
 
-		return nil
-	},
+	return nil
 }
--- go.mod
+++ go.mod
@@ -17,16 +17,34 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.3.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.0.0 // indirect
 	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/cilium/ebpf v0.7.0 // indirect
+	github.com/cosiner/argv v0.1.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/derekparker/trie v0.0.0-20221213183930-4c74548207f4 // indirect
+	github.com/go-delve/delve v1.21.0 // indirect
+	github.com/go-delve/liner v1.2.3-0.20220127212407-d32d89dd2a5d // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/google/go-dap v0.9.1 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
 	github.com/stretchr/testify v1.8.3 // indirect
+	go.starlark.net v0.0.0-20220816155156-cfacd8902214 // indirect
+	golang.org/x/arch v0.0.0-20190927153633-4e8777c89be4 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
