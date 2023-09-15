092420fc9e2dcbaf1923114182dc6f2764b0868c
Jaiveer Katariya <jveerkatariya@gmail.com>
2023-09-15 17:24:27 -0400

    dont like sp

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"github.com/azure/spin-aks-plugin/pkg/azure"
-)
+import "github.com/azure/spin-aks-plugin/cmd"
 
 var version string // set through ldflags at build time like "go run -ldflags "-X main.version=0.0.1" main.go"
 
 func main() {
-	err := azure.LinkAcr(context.Background(), "26ad903f-2330-429d-8389-864ac35c4350", "jkatariyatest", "jkatariyatest5", "jkatariyatest", "jkatariyatest")
-	fmt.Println(err)
+	cmd.Execute(cmd.Config{
+		Version: version,
+	})
 }
